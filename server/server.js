c20785e3bff927a1ec95554efadb440797d49675
pavlvoic265 <pavlvoic265@gmail.com>
2017-05-13 17:15:46 +0200

    User leaving fix

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ io.on('connection', (socket) => {
         var user = users.removeUser(socket.id);
 
         if(user) {
-            io.to(user.room).emit('updateUserList', users.getUserList(users.room));
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
             io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
         }
     });
